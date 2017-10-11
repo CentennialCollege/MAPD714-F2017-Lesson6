133a72cfa4a2c14d191eacb13ab6d103476e809e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-10-11 19:57:00 -0400

    Completed Date Scene

--- MAPD714-F2017-Lesson6/DatePickerViewController.swift
+++ MAPD714-F2017-Lesson6/DatePickerViewController.swift
@@ -2,7 +2,32 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let date = NSDate()
+        datePicker.setDate(date as Date, animated: false)
     }
+    
+    @IBAction func OnButtonPressed(_ sender: UIButton) {
+        let date = datePicker.date
+        
+        let message = "The Date and Time you selected is \(date)"
+        
+        let alert = UIAlertController(
+            title: "Date and Time Selected",
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "That's So True!",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
